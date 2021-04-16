124ed53bb0bbe3e64dd7135839654969c50fc307
tonywu <h0980723509@gmail.com>
2021-04-16 21:11:12 +0800

    'test'

--- app/Http/Controllers/houseController.php
+++ app/Http/Controllers/houseController.php
@@ -9,27 +9,22 @@ use Illuminate\Support\Facades\DB;
 class houseController extends Controller
 {
     public function index(){
-        // $db = DB::connection('mysql');
-        $house = DB::table('house')
-                ->where('house_is_del', '=', 0)
-                ->where('house_is_img', '=', 1)
-                ->orderBy('house_id', 'desc')
-                ->paginate(5);
-        // $house = $db->table('house')
-        //            ->where('house_is_del', '=', 0)
-        //            ->where('house_is_img', '=', 1)
-        //            ->orderBy('price', 'asc')
-        //            ->get()->paginate(15)->toArray();
-
-        $temp = DB::table('house_img')->get();
-        // $temp = $db->table('house_img')
-                //   ->get()->toArray();
-
-
+        $test = DB::table('test')-get();
+        $house = array();
         $pic = array();
-        foreach($temp as $key => $value){
-            $pic[$value->house_id][] = $value->house_img_name;
-        }
+        // $db = DB::connection('mysql');
+        // $house = DB::table('house')
+        //         ->where('house_is_del', '=', 0)
+        //         ->where('house_is_img', '=', 1)
+        //         ->orderBy('house_id', 'desc')
+        //         ->paginate(5);
+
+        // $temp = DB::table('house_img')->get();
+
+        // $pic = array();
+        // foreach($temp as $key => $value){
+        //     $pic[$value->house_id][] = $value->house_img_name;
+        // }
 
         return view('welcome', ['house' => $house, 'pic' => $pic]);
     }
