8fc4bbee162788c71c4ec131c57803afc997e44d
tonywu <h0980723509@gmail.com>
2021-04-18 11:53:57 +0800

    'asdf

--- public/youtube.py
+++ public/youtube.py
@@ -1,10 +1,11 @@
-from pytube import YouTube
-import shutil
-import sys
+# from pytube import YouTube
+# import shutil
+# import sys
 import os
 
-if len(sys.argv) >= 2:
-    folderpath = "C:/xampp/htdocs/stock/public/mp4"
-    if os.path.isdir(folderpath):
-        shutil.rmtree(folderpath)
-    YouTube(sys.argv[1]).streams.first().download('C:/xampp/htdocs/stock/public/mp4')
+os.makedirs('test')
+# if len(sys.argv) >= 2:
+#     folderpath = "C:/xampp/htdocs/stock/public/mp4"
+#     if os.path.isdir(folderpath):
+#         shutil.rmtree(folderpath)
+#     YouTube(sys.argv[1]).streams.first().download('C:/xampp/htdocs/stock/public/mp4')
