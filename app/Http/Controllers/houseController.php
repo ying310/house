f195959daf14a7466e39fa5c1260a10a5fce39f5
tonywu <h0980723509@gmail.com>
2021-04-20 20:06:24 +0800

    'asdf'

--- app/Http/Controllers/houseController.php
+++ app/Http/Controllers/houseController.php
@@ -30,14 +30,11 @@ class houseController extends Controller
     }
 
     public function update(){
-        exec('python3 /var/www/house/public/house.py 2>&1', $result, $out);
-        print_r($result);
-        echo '<br>';
-        print_r($out);
+        // exec('python3 /var/www/house/public/house.py 2>&1', $result, $out);
         // sleep(2);
-        // exec('python3 /var/www/house/public/getHouseImg.py');
-        // Session::flash('status', '更新成功');
-        // return redirect('/');
+        exec('python3 /var/www/house/public/getHouseImg.py');
+        Session::flash('status', '更新成功');
+        return redirect('/');
     }
 
     public function youtubelist(){
